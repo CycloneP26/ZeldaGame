9eccf9ecb60a330f544ce37132f5a624d8413f27
CycloneP26 <70384658+CycloneP26@users.noreply.github.com>
2023-05-11 15:54:49 -0400

    Add files via upload

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -2,65 +2,63 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class GamePanel extends JPanel implements Runnable
 {
-	final int originalTileSize=16; //tile size
+	final int originalTileSize=16;
 	final int scale=3;
-	public final int tileSize=originalTileSize*scale; 
-	final int maxScreenCol=16; //column # of screen
-	final int maxScreenRow=16; //row # of screen
-	final int screenWidth=tileSize*maxScreenCol; //screen width
-	final int screenHeight=tileSize*maxScreenRow; //screen height
-	private Timer timer; //timer
-	private int currentRoomRow; //current row in the room
-	private int currentRoomColumn; //current column in the room
-	
-	int FPS=60;
+	final int tileSize=originalTileSize*scale;
+	final int maxScreenCol=16;
+	final int maxScreenRow=12;
+	final int screenWidth=tileSize*maxScreenCol;
+	final int screenHeight=tileSize*maxScreenRow;
+	private Timer timer;
+	private int FPS = 60;
 	
 	
-	
-	private RoomManager rooms; //room manager
-	private KeyHandler keyH; //keyhandler
-	private Thread gameThread; 
-	private Player player; //Link
-	private CollisionChecker cChecker; //collision checker
+	private RoomManager rooms;
+	private KeyHandler keyH;
+	private Thread gameThread;
+	private Player player;
+	private CollisionChecker cChecker;
 	
 	
 	public GamePanel()
 	{
-		//initializes everything
+		
 		keyH=new KeyHandler();
+		
 		rooms = new RoomManager(this, 3, 3);
+		
 		player=new Player(this,keyH);
-		currentRoomRow = 1;
-		currentRoomColumn = 1;
-		cChecker = new CollisionChecker(this, rooms, currentRoomRow, currentRoomColumn);
+		
+		cChecker = new CollisionChecker(this, rooms, rooms.getRoomRow(), rooms.getRoomColumn());
+		
 		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
 		this.setBackground(Color.white);
 		this.setDoubleBuffered(true);
 		this.addKeyListener(keyH);
 		this.setFocusable(true);
+		
 	}
 	
-	//returns collision checker status
-	public CollisionChecker getCollision()
-	{
-		return cChecker;
-	}
-	
-	
-	//start game
 	public void startGameThread()
 	{
+		
 		gameThread=new Thread(this);
 		gameThread.start();
+		
 	}
 	
-	//runs the game, updating every movement and where Link is 
+	public CollisionChecker getCollision()
+	{
+		
+		return cChecker;
+		
+	}
+	
 	public void run()
 	{
 		double drawInterval=1000000000/FPS;
@@ -69,75 +67,10 @@ public class GamePanel extends JPanel implements Runnable
 		while(gameThread!=null)
 		{
 			update();
-				
-			// Check if the player has moved off the top edge of the screen. If yes:
-			if(player.getY() < 0)
-			{
-				// Set the player's y-coordinate to the bottom of the screen, offset by 40 pixels.
-				player.setY(screenHeight - 40);
-				
-				// Check if there is a room available above the current room. If yes:
-				if(rooms.isRoomAvailable(currentRoomRow - 1, currentRoomColumn))
-				{
-					// Set the current room row to one row above the current room.
-					currentRoomRow--;
-					
-					// Update the current row value in the collision checker.
-					cChecker.setCurRow(currentRoomRow);
-				}
-			}
-
-			//checks if player has moved to the left of the screen. if yes
-			else if(player.getX()<0)
-			{
-				//set the player's x coordinate to the right edge of the screen
-				player.setX(screenWidth-40);
-				// Check if there is a room available to the left of the current room. If yes:
-				if(rooms.isRoomAvailable(currentRoomRow, currentRoomColumn - 1))
-				{
-					// Set the current room column to one column to the left of the current room.
-					currentRoomColumn--;
-					
-					// Update the current column value in the collision checker.
-					cChecker.setCurCol(currentRoomColumn);
-				}
-
-			}
-			//checks if the player has moved to the bottom of the screen. if yes
-			else if(player.getY() + 40>screenHeight)
-			{
-				//move the players y coordinate to the top of the screen
-				player.setY(0);
-				// Check if there is a room available below the current room. If yes:
-				if(rooms.isRoomAvailable(currentRoomRow + 1, currentRoomColumn))
-				{
-					// Set the current room row to one row below the current room.
-					currentRoomRow++;
-					
-					// Update the current row value in the collision checker.
-					cChecker.setCurRow(currentRoomRow);
-				}
-			}
-			//check if the player has moved to the right of the screen. if yes
-			else if(player.getX()+40>screenWidth)
-			{
-				//set the players x coordinates to the left of the screen
-				player.setX(0);
-				// Check if there is a room available to the right of the current room. If yes:
-				if(rooms.isRoomAvailable(currentRoomRow, currentRoomColumn+1))
-				{
-					// Set the current room column to one column to the right of the current room.
-					currentRoomColumn++;
-					
-					// Update the current column value in the collision checker.
-					cChecker.setCurCol(currentRoomColumn);
-				}
-			}
-			}
-			
-			repaint(); //update the room
 			
 			
+			
+			repaint();
 			
 			try {
 				double remainingTime=nextDrawTime-System.nanoTime();
@@ -157,20 +90,32 @@ public class GamePanel extends JPanel implements Runnable
 			
 			
 		}
+	}
 	
-	//update
+	
 	public void update()
 	{
+		
 		player.update();
+		rooms.update();
+		
 	}
-	//paint the room
+	
+	public Player getPlayer()
+	{
+		
+		return player;
+		
+	}
+	
 	public void paintComponent(Graphics g)
 	{
+		
 		super.paintComponent(g);
 		
 		Graphics2D g2=(Graphics2D)g;
 		
-		rooms.getRoomArray().get(currentRoomRow).get(currentRoomColumn).draw(g2);
+		rooms.getRoomArray().get(rooms.getRoomRow()).get(rooms.getRoomColumn()).draw(g2);
 		player.draw(g2);
 		g2.dispose();
 		
--- src/Tile.java
+++ src/Tile.java
@@ -3,7 +3,6 @@ import java.awt.image.BufferedImage;
 
 public class Tile {
 	
-	//tile pictures and collision status
 	public BufferedImage image;
 	public boolean collision = false;
 	
