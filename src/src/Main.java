d0b8ddf7f4552d0c68fc6d4151294a7b4c0b5623
sachhayaKO <132700991+sachhayaKO@users.noreply.github.com>
2023-05-15 14:02:55 -0400

    Add files via upload

---
+++ src/src/Main.java
@@ -0,0 +1,30 @@
+import javax.swing.JFrame;
+import javax.swing.Timer;
+
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
+
+public class Main extends JFrame
+{
+	public static void main(String[]args)
+	{
+		
+		JFrame window=new JFrame();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(false);
+		window.setTitle("The Legend of Zelda");
+		
+		GamePanel gamePanel=new GamePanel();
+		window.add(gamePanel);
+		
+		window.pack();
+		window.setLocationRelativeTo(null);
+		window.setVisible(true);
+		
+		gamePanel.startGameThread();
+		
+	}
+}
