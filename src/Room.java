94ac178f9ba9c6e2c143dba861b8eb7c3f5d7876
Chr1spu <132961476+Chr1spu@users.noreply.github.com>
2023-05-18 11:22:55 -0400

    Add files via upload

--- src/Room.java
+++ src/Room.java
@@ -5,16 +5,16 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 public class Room {
 	
-	private GamePanel gp;
-	private Tile[] tile;
-	private int[][] tileLayout;
+	GamePanel gp;
+	Tile[] tile;
+	int[][] tileLayout = new int[12][16];
 	
 	public Room(GamePanel gp)
 	{
 		this.gp = gp;
 		
 		tile = new Tile[10];
-		tileLayout = new int[12][16];
+		
 		getTileImage();
 		
 		for(int i = 0; i<tileLayout.length; i++)
@@ -26,48 +26,20 @@ public class Room {
 		}
 		
 	}
-	public Room(GamePanel gp, String str)
-	{
-		this.gp = gp;
-		
-		tile = new Tile[10];
-		tileLayout = new int[12][16];
-		getTileImage();
-		
-		tileLayout = strToMap(str);
-		
-	}
-	
-	public int[][] strToMap(String str)
-	{
-		int[][] retArray = new int[12][16];
-		int count = 0;
-		for(int i = 0; i<12; i++)
-		{
-			for(int j = 0; j<16; j++)
-			{
-				retArray[i][j]+=Integer.parseInt(str.substring(count, count+1));
-				count++;
-			}
-		}
-		
-		
-		return retArray;
-	}
 	
 	public void getTileImage()
 	{
 		try
 		{
 			tile[0] = new Tile();
-			tile[0].setImage(ImageIO.read(getClass().getResourceAsStream("/tiles/groundWithDots.png")));
+			tile[0].image = ImageIO.read(getClass().getResourceAsStream("/tiles/groundWithDots.png"));
 			
 			tile[1] = new Tile();
-			tile[1].setImage(ImageIO.read(getClass().getResourceAsStream("/tiles/download.png")));
-			tile[1].setCollision(true);
+			tile[1].image = ImageIO.read(getClass().getResourceAsStream("/tiles/download.png"));
+			tile[1].collision = true;
 			
 			tile[2] = new Tile();
-			tile[2].setImage(ImageIO.read(getClass().getResourceAsStream("/tiles/water.png")));
+			tile[2].image = ImageIO.read(getClass().getResourceAsStream("/tiles/water.png"));
 		}
 		catch(IOException e)
 		{
@@ -82,18 +54,13 @@ public class Room {
 		
 	}
 	
-	public Tile[] getTile()
-	{
-		return tile;
-	}
-	
 	public void draw(Graphics2D g2)
 	{
 		for(int i = 0; i<tileLayout.length; i++)
 		{
 			for(int j = 0; j<tileLayout[0].length; j++)
 			{
-				g2.drawImage(tile[tileLayout[i][j]].getImage(), j*48, i*48, gp.tileSize, gp.tileSize, null);
+				g2.drawImage(tile[tileLayout[i][j]].image, j*48, i*48, gp.tileSize, gp.tileSize, null);
 			}
 		}
 	}
