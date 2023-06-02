83f1295b43be09d6888803f6a9ec4e27a5375527
sachhayaKO <sachhaya1@gmail.com>
2023-06-02 14:18:24 -0400

    Revert "Merge pull request #18 from CycloneP26/sachhayaKO-item-path"
    This reverts commit 94af8994eb4ab3c1fb409b81396f4c26cfe5454c, reversing
    changes made to 14f73bab0e183d11ac06823a0aeb890ffd1271cb.

--- src/object/Bomb.java
+++ src/object/Bomb.java
@@ -1,14 +1,29 @@
 package object;
-import Main.Entity;
-import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
+
 import java.io.IOException;
+
 import javax.imageio.ImageIO;
 import javax.swing.Timer;
-import Main.GamePanel;
-import Main.UtilityTool;
-public class Bomb extends ItemEntity
+
+public class Bomb extends ObjectMain
 {
-	
-//Radius of the bomb blast 
+	private final int EXPLOSIONRAD = 16; //Radius of the bomb blast 
+	private Timer timer;
+	public Bomb()
+	{
+		name = "Bomb";
+		try //Used to negate error for image pathing 
+		{
+			//Image insertion for the Bomb
+			ImageIO.read(getClass().getResourceAsStream("/objects/Bomb.png"));
+		}
+		catch(IOException e) 
+		{
+			e.printStackTrace();
+		}
+	}
+	public int getRad()
+	{
+		return EXPLOSIONRAD;
+	}
 }
