2a6755e6c4e2507099c06621a13d87a51818184d
CycloneP26 <70384658+CycloneP26@users.noreply.github.com>
2023-06-03 14:42:37 -0400

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,7 @@ public class Main extends JFrame
 		
 		JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setSize(768, 768);
+        window.setSize(768, 757);
         window.setResizable(false);
         window.setTitle("The Legend of Zelda");
 
