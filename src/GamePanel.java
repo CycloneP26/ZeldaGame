5fca0098e770e6b4a27364d3d5b206d816842298
sachhayaKO <132700991+sachhayaKO@users.noreply.github.com>
2023-06-05 14:04:20 -0400

    Update GamePanel.java

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -18,15 +18,17 @@ public class GamePanel extends JPanel implements Runnable
 	final int screenHeight=tileSize*maxScreenRow;
 	private int FPS = 60;
 	private Timer timer;
-    private int hudHeight = 16 * 3; // height of the HUD panel
 	
+    	private int hudHeight = 16 * 3; // height of the HUD panel
 	
+	private AssetSetter assetS = new AssetSetter(this);
 	private RoomManager rooms;
 	private KeyHandler keyH;
 	private Thread gameThread;
 	private Player player;
 	private CollisionChecker cChecker;
 	private Entity mobs[];
+	private ItemEntity obj[] = new ItemEntity[10];
 
 	
 	
@@ -53,7 +55,10 @@ public class GamePanel extends JPanel implements Runnable
 		this.setFocusable(true);
 		
 	}
-	
+	public void setUpGame()
+	{
+		assetS.setObj();
+	}
 	public void startGameThread()
 	{
 		
@@ -185,5 +190,11 @@ public class GamePanel extends JPanel implements Runnable
 	public void setMobs(Entity mobs[]) {
 		this.mobs = mobs;
 	}
-
+	public ItemEntity[] getItems() 
+	{
+		return items;
+	}
+	public void setItems(ItemEntity items[]) {
+		this.items = items;
+	}
 }
