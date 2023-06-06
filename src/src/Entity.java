59c936421f5a091b808deb6d15971b7dd472fb7c
DirtyDavee77 <132941801+DirtyDavee77@users.noreply.github.com>
2023-06-06 18:19:31 -0400

    Add files via upload

---
+++ src/src/Entity.java
@@ -0,0 +1,415 @@
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.IOException;
+import java.awt.Graphics2D;
+
+public class Entity {
+	private GamePanel gp;
+	private int worldX,worldY;
+	private int speed;
+	private BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
+	private BufferedImage swordUp,swordUp1,swordLeft,swordLeft1,swordRight,swordRight1,
+	swordDown,swordDown1, itemUp, itemDown, itemRight, itemLeft;
+	private BufferedImage OctorokUp,OctorokUp1,OctorokDown,OctorokDown1,OctorokLeft,OctorokLeft1,OctorokRight,OctorokRight1;
+	private BufferedImage SpiderStill,SpiderJump;
+	private BufferedImage Rock;
+	private String direction;
+	private String direction2;
+	
+	public int spriteCounter=0;
+	public int spriteNum=1;
+	boolean attacking=false;
+	boolean bomb = false;
+	boolean itemUse = false;
+	
+	private Rectangle solidArea=new Rectangle(0,0,48,48);
+	private boolean collisionOn = false;
+	private int actionLockCounter=0;
+
+	
+	
+	public Entity(GamePanel gp)
+	{
+		this.setGp(gp);
+	}
+	public void setAction()
+	{
+
+	}
+	public void update()
+	{
+		setAction();
+		collisionOn=false;
+		getGp().getCollision().checkTile(this);
+		if(isCollisionOn() == false)
+		{
+			switch(getDirection())
+			{
+			case "up":
+				if(worldX>0&&worldY>0&&worldX<700&&worldY<700)
+				{
+					if(worldY-speed>0)
+					{
+						worldY-=speed;
+					}
+					else
+					{
+						setAction();
+					}
+				}
+				
+				break;
+			case "down":
+				if(worldX>0&&worldY>0&&worldX<700&&worldY<700)
+				{
+					if(worldY+speed<500)
+					{
+						worldY+=speed;
+					}
+					else
+					{
+						setAction();
+					}
+				}
+				
+				break;
+			case "left":
+				if(worldX>0&&worldY>0&&worldX<700&&worldY<700)
+				{
+					if(worldX-speed>0)
+					{
+						worldX-=speed;
+					}
+					else
+					{
+						setAction();
+					}
+				}
+				
+				break;
+			case "right":
+				if(worldX>0&&worldY>0&&worldX<700&&worldY<700)
+				{
+					if(worldX+speed<700)
+					{
+						worldX+=speed;
+					}
+					else
+					{
+						setAction();
+					}
+				}
+				
+				break;
+
+			}
+
+		}
+		spriteCounter++;
+		if(spriteCounter>12)
+		{
+			if(spriteNum==1)
+			{
+				spriteNum=2;
+			}
+			else if(spriteNum==2)
+			{
+				spriteNum=1;
+			}
+			spriteCounter=0;
+		}
+	}
+	public void draw(Graphics2D g2) 
+	{
+		BufferedImage image=null;
+
+		//worldY-getGp().getPlayer().getY()+getGp().getPlayer().getScreenY();
+		//if(worldX+getGp().getTileSize()>getGp().getPlayer().getX()-getGp().getPlayer().getScreenX() &&
+		  // worldX-getGp().getTileSize()<getGp().getPlayer().getX()+getGp().getPlayer().getScreenX()&&
+		  // worldY+getGp().getTileSize()>getGp().getPlayer().getY()-getGp().getPlayer().getScreenY()&&
+		  // worldY-getGp().getTileSize()<getGp().getPlayer().getY()+getGp().getPlayer().getScreenY())
+		{
+			switch(getDirection()) {
+			case "up":
+				if(spriteNum==1){image=getUp1();}
+				if(spriteNum==2){image=getUp2();}
+				break;
+			case "down":
+				if(spriteNum==1){image=getDown1();}
+				if(spriteNum==2){image=getDown2();}
+				break;
+			case "left":
+				if(spriteNum==1){image=getLeft1();}
+				if(spriteNum==2){image=getLeft2();}
+				break;
+			case "right":
+				if(spriteNum==1){image=getRight1();}
+				if(spriteNum==2){image=getRight2();}
+				break;
+			}
+			g2.drawImage(image,worldX,worldY,gp.getTileSize(),gp.getTileSize(),null);
+		}
+	}
+	public int getTileSize()
+	{
+		return getGp().tileSize;
+	}
+	public int getX()
+	{
+		return worldX;
+	}
+	public int getY()
+	{
+		return worldY;
+	}
+	public void setX(int x)
+	{
+		this.worldX = x;
+	}
+	public void setY(int y)
+	{
+		this.worldY = y;
+	}
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	public BufferedImage getOctorokUp() {
+		return OctorokUp;
+	}
+	public void setOctorokUp(BufferedImage b)
+	{
+		this.OctorokUp=b;
+	}
+	public BufferedImage getOctorokUp1() {
+		return OctorokUp1;
+	}
+	public void setOctorokUp1(BufferedImage b)
+	{
+		this.OctorokUp1=b;
+	}
+	public BufferedImage getOctorokDown() {
+		return OctorokDown;
+	}
+	public void setOctorokDown(BufferedImage b)
+	{
+		this.OctorokDown=b;
+	}
+	public BufferedImage getOctorokDown1() {
+		return OctorokDown1;
+	}
+	public void setOctorokDown1(BufferedImage b)
+	{
+		this.OctorokDown1=b;
+	}
+	public BufferedImage getOctorokLeft() {
+		return OctorokLeft;
+	}
+	public void setOctorokLeft(BufferedImage b)
+	{
+		this.OctorokLeft=b;
+	}
+	public BufferedImage getOctorokLeft1() {
+		return OctorokLeft1;
+	}
+	public void setOctorokLeft1(BufferedImage b)
+	{
+		this.OctorokLeft1=b;
+	}
+	public BufferedImage getOctorokRight() {
+		return OctorokRight;
+	}
+	public void setOctorokRight(BufferedImage b)
+	{
+		this.OctorokRight=b;
+	}
+	public BufferedImage getOctorokRight1() {
+		return OctorokRight1;
+	}
+	public void setOctorokRight1(BufferedImage b)
+	{
+		this.OctorokRight1=b;
+	}
+	public BufferedImage getUp1() {
+		return up1;
+	}
+	public void setUp1(BufferedImage up1) {
+		this.up1 = up1;
+	}
+	public BufferedImage getUp2() {
+		return up2;
+	}
+	public void setUp2(BufferedImage up2) {
+		this.up2 = up2;
+	}
+	public BufferedImage getDown1() {
+		return down1;
+	}
+	public void setDown1(BufferedImage down1) {
+		this.down1 = down1;
+	}
+	public BufferedImage getDown2() {
+		return down2;
+	}
+	public void setDown2(BufferedImage down2) {
+		this.down2 = down2;
+	}
+	public BufferedImage getLeft1() {
+		return left1;
+	}
+	public void setLeft1(BufferedImage left1) {
+		this.left1 = left1;
+	}
+	public BufferedImage getLeft2() {
+		return left2;
+	}
+	public void setLeft2(BufferedImage left2) {
+		this.left2 = left2;
+	}
+	public BufferedImage getRight1() {
+		return right1;
+	}
+	public void setRight1(BufferedImage right1) {
+		this.right1 = right1;
+	}
+	public BufferedImage getRight2() {
+		return right2;
+	}
+	public void setRight2(BufferedImage right2) {
+		this.right2 = right2;
+	}
+	public BufferedImage getSwordUp() {
+		return swordUp;
+	}
+	public void setSwordUp(BufferedImage swordUp) {
+		this.swordUp = swordUp;
+	}
+	public BufferedImage getSwordUp1() {
+		return swordUp1;
+	}
+	public void setSwordUp1(BufferedImage swordUp1) {
+		this.swordUp1 = swordUp1;
+	}
+	public BufferedImage getSwordLeft() {
+		return swordLeft;
+	}
+	public void setSwordLeft(BufferedImage swordLeft) {
+		this.swordLeft = swordLeft;
+	}
+	public BufferedImage getSwordLeft1() {
+		return swordLeft1;
+	}
+	public void setSwordLeft1(BufferedImage swordLeft1) {
+		this.swordLeft1 = swordLeft1;
+	}
+	public BufferedImage getSwordRight() {
+		return swordRight;
+	}
+	public void setSwordRight(BufferedImage swordRight) {
+		this.swordRight = swordRight;
+	}
+	public BufferedImage getSwordRight1() {
+		return swordRight1;
+	}
+	public void setSwordRight1(BufferedImage swordRight1) {
+		this.swordRight1 = swordRight1;
+	}
+	public BufferedImage getSwordDown() {
+		return swordDown;
+	}
+	public void setSwordDown(BufferedImage swordDown) {
+		this.swordDown = swordDown;
+	}
+	public BufferedImage getSwordDown1() {
+		return swordDown1;
+	}
+	public void setSwordDown1(BufferedImage swordDown1) {
+		this.swordDown1 = swordDown1;
+	}
+	public String getDirection() {
+		return direction;
+	}
+	public void setDirection(String direction) {
+		this.direction = direction;
+	}
+	public BufferedImage getItemUp(){return itemUp;}
+	public BufferedImage getItemDown(){return itemDown;}
+	public BufferedImage getItemLeft(){return itemLeft;}
+	public BufferedImage getItemRight(){return itemRight;}
+	public void setItemUp(BufferedImage img) {this.itemUp = img;}
+	public void setItemDown(BufferedImage img) {this.itemDown = img;}
+	public void setItemLeft(BufferedImage img) {this.itemLeft = img;}
+	public void setItemRight(BufferedImage img) {this.itemRight = img;}
+
+
+	public Rectangle getSolidArea() {
+		return solidArea;
+	}
+
+	public void setSolidArea(Rectangle solidArea) {
+		this.solidArea = solidArea;
+	}
+
+	public boolean isCollisionOn() {
+		return collisionOn;
+	}
+
+	public void setCollisionOn(boolean collisionOn) {
+		this.collisionOn = collisionOn;
+	}
+	public BufferedImage setup(String imagePath,int width,int height)
+	{
+		UtilityTool uTool=new UtilityTool();
+		BufferedImage image=null;
+		try
+		{
+			image=ImageIO.read(getClass().getResourceAsStream(imagePath+".png"));
+			image=uTool.scaleImage(image,width,height);
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		return image;
+	}
+	public GamePanel getGp() {
+		return gp;
+	}
+	public void setGp(GamePanel gp) {
+		this.gp = gp;
+	}
+	public int getActionLockCounter() {
+		return actionLockCounter;
+	}
+	public void setActionLockCounter(int actionLockCounter) {
+		this.actionLockCounter = actionLockCounter;
+	}
+	public BufferedImage getSpiderStill() {
+		return SpiderStill;
+	}
+	public void setSpiderStill(BufferedImage spiderStill) {
+		SpiderStill = spiderStill;
+	}
+	public BufferedImage getSpiderJump() {
+		return SpiderJump;
+	}
+	public void setSpiderJump(BufferedImage spiderJump) {
+		SpiderJump = spiderJump;
+	}
+	public String getDirection2() {
+		return direction2;
+	}
+	public void setDirection2(String direction2) {
+		this.direction2 = direction2;
+	}
+	
+	public BufferedImage getRock() {
+		return Rock;
+	}
+	public void setRock(BufferedImage rock) {
+		Rock = rock;
+	}
+}
---
+++ src/src/Room.java
@@ -0,0 +1,216 @@
+
+import java.awt.Graphics2D;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
+public class Room {
+	
+	
+	private GamePanel gp;
+	private Tile[] tile;
+	private int[][] tileLayout;
+	private String str;
+	private ArrayList<Entity> mobs;
+	
+	public Room(GamePanel gp)
+	{
+		this.gp = gp;
+		
+		tile = new Tile[30];
+		
+		getTileImage();
+		
+		for(int i = 0; i<tileLayout.length; i++)
+		{
+			for(int j = 0; j<tileLayout[0].length; j++)
+			{
+				tileLayout[i][j] = (int)(Math.random()*3);
+			}
+		}
+
+		str = mapToStr(tileLayout);
+		mobs = new ArrayList<Entity>();
+		
+	}
+	public Room(GamePanel gp, String str)
+	{
+		this.gp = gp;
+		this.str = str;
+		
+		tile = new Tile[30];
+		tileLayout = new int[12][16];
+		getTileImage();
+		
+		tileLayout = strToMap(str);
+		mobs = new ArrayList<Entity>();
+		
+	}
+	
+	public int[][] strToMap(String str)
+	{
+		int[][] retArray = new int[12][16];
+		int count = 0;
+		for(int i = 0; i<12; i++)
+		{
+			for(int j = 0; j<16; j++)
+			{
+				if(str.substring(count, count+1) != "\n" || str.substring(count, count+1) != "\r")
+				{
+					
+					//retArray[i][j]+=Integer.parseInt(str.substring(count, count+1));
+					retArray[i][j]+= convertToNum(str.substring(count, count+1));
+					
+				}
+				
+				count++;
+			}
+		}
+		
+		
+		return retArray;
+	}
+
+	public String mapToStr(int[][] map)
+	{
+		String retStr = "";
+		for(int i = 0; i<map.length; i++)
+		{
+			for(int j = 0; j<map[0].length; j++)
+			{
+				retStr += map[i][j];
+			}
+		}
+		return retStr;
+	}
+	
+	public void getTileImage()
+	{
+		try
+		{
+			tile[0] = new Tile();
+			tile[0].image = ImageIO.read(getClass().getResourceAsStream("/tiles/groundWithDots.png"));
+			
+			tile[1] = new Tile();
+			tile[1].image = ImageIO.read(getClass().getResourceAsStream("/tiles/download.png"));
+			tile[1].collision = true;
+			
+			tile[2] = new Tile();
+			tile[2].image = ImageIO.read(getClass().getResourceAsStream("/tiles/water.png"));
+			tile[2].collision = true;
+			
+			tile[3] = new Tile();
+			tile[3].image = ImageIO.read(getClass().getResourceAsStream("/tiles/clearFlatGround.png"));
+
+			tile[4] = new Tile();
+			tile[4].image = ImageIO.read(getClass().getResourceAsStream("/tiles/middleRock.png"));
+			tile[4].collision = true;
+			
+			tile[5] = new Tile();
+			tile[5].image = ImageIO.read(getClass().getResourceAsStream("/tiles/greenWall.png"));
+			tile[5].collision = true;
+			
+			tile[6] = new Tile();
+			tile[6].image = ImageIO.read(getClass().getResourceAsStream("/tiles/greenDiagOne.png"));
+			tile[6].collision = true;
+			
+			tile[7] = new Tile();
+			tile[7].image = ImageIO.read(getClass().getResourceAsStream("/tiles/greenDiagTwo.png"));
+			tile[7].collision = true;
+			
+			tile[8] = new Tile();
+			tile[8].image = ImageIO.read(getClass().getResourceAsStream("/tiles/black.png"));
+			
+			tile[9] = new Tile();
+			tile[9].image = ImageIO.read(getClass().getResourceAsStream("/tiles/cactus.png"));
+			tile[9].collision = true;
+			
+			tile[10] = new Tile();
+			tile[10].image = ImageIO.read(getClass().getResourceAsStream("/tiles/cactus.png"));
+			tile[10].collision = true;
+			
+			//a
+			tile[11] = new Tile();
+			tile[11].image = ImageIO.read(getClass().getResourceAsStream("/tiles/rockDiagOne.png"));
+			tile[11].collision = true;
+			
+			//b
+			tile[12] = new Tile();
+			tile[12].image = ImageIO.read(getClass().getResourceAsStream("/tiles/rockDiagTwo.png"));
+			tile[12].collision = true;
+			
+			//c
+			tile[13] = new Tile();
+			tile[13].image = ImageIO.read(getClass().getResourceAsStream("/tiles/rockDiagThree.png"));
+			tile[13].collision = true;
+			
+			//d
+			tile[14] = new Tile();
+			tile[14].image = ImageIO.read(getClass().getResourceAsStream("/tiles/rockDiagFour.png"));
+			tile[14].collision = true;
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public int[][] getTileLayout()
+	{
+		
+		return tileLayout;
+		
+	}
+	
+	public int convertToNum(String s)
+	{
+		char c = s.charAt(0);
+		if(Character.isDigit(c))
+		{
+			return Integer.parseInt(s);
+		}
+		else
+		{
+			char temp = c;
+			int count = 0;
+			while(temp != 'a')
+			{
+				temp--;
+				count++;
+			}
+			return count + 11;
+		}
+	}
+	
+	public ArrayList<Entity> getMobs()
+	{
+		return mobs;
+	}
+	
+	public void addMobs(Entity e)
+	{
+		mobs.add(e);
+	}
+	
+	public Tile[] getTile()
+	{
+		return tile;
+	}
+	
+	public void draw(Graphics2D g2)
+	{
+		for(int i = 0; i<tileLayout.length; i++)
+		{
+			for(int j = 0; j<tileLayout[0].length; j++)
+			{
+				g2.drawImage(tile[tileLayout[i][j]].image, j*48, i*48, gp.tileSize, gp.tileSize, null);
+			}
+		}
+	}
+	public String getStr() {
+		return str;
+	}
+	public void setStr(String str) {
+		this.str = str;
+	}
+}
