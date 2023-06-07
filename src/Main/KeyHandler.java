35a4995e9f182f9406811c9ed37b2dff1bd85bf5
sachhayaKO <132700991+sachhayaKO@users.noreply.github.com>
2023-06-07 10:44:33 -0400

    Update KeyHandler.java

--- src/Main/KeyHandler.java
+++ src/Main/KeyHandler.java
@@ -5,10 +5,10 @@ import java.awt.event.KeyListener;
 
 public class KeyHandler implements KeyListener
 {
-	private boolean upPressed, downPressed, leftPressed, rightPressed, swordPressed, bItem;
+	private boolean upPressed, downPressed, leftPressed, rightPressed, swordPressed, bItem; //All booleans that determine if a key is pressed
 	private boolean on;
 	
-	public KeyHandler()
+	public KeyHandler() //Calls the super constructor for the KeyListener class, and it turns on the ability to observe keystrokes
 	{
 
 		super();
@@ -16,7 +16,7 @@ public class KeyHandler implements KeyListener
 
 	}
 
-	public void setOn(boolean on)
+	public void setOn(boolean on) //@param boolean to change whether it is on or off
 	{
 
 		this.on = on;
@@ -30,9 +30,11 @@ public class KeyHandler implements KeyListener
 		// TODO Auto-generated method stub
 		
 	}
-
+	/*
+	When a key is pressed, it changes the booleans that show which key is pressed
+	*/
 	@Override
-	public void keyPressed(KeyEvent e) 
+	public void keyPressed(KeyEvent e) //@param keystroke argument is passed 
 	{
 		int code=e.getKeyCode();
 		if(on == true)
@@ -66,9 +68,9 @@ public class KeyHandler implements KeyListener
 		}
 		
 	}
-
+	//When a keystroke is released, it makes the booleans false 
 	@Override
-	public void keyReleased(KeyEvent e) 
+	public void keyReleased(KeyEvent e) //@param takes the keystroke argument 
 	{
 		int code=e.getKeyCode();
 		if(code==KeyEvent.VK_W)
