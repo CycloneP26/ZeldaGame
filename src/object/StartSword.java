4bbddf6cac91fb068ed6ca616a30c8ac92220e5d
sachhayaKO <132700991+sachhayaKO@users.noreply.github.com>
2023-06-09 01:02:27 -0400

    Update StartSword.java

--- src/object/StartSword.java
+++ src/object/StartSword.java
@@ -5,10 +5,15 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 import Main.GamePanel;
-
+/*
+Creates an item that is able to be picked up by Link
+It is the original sword that is found in the cave
+extends ItemEntity to be able to be picked up 
+@author Sachin Chhaya 
+*/
 public class StartSword extends ItemEntity
 {
-
+	//Not going to be used (base Constructor) 
 	public StartSword(GamePanel gp)
 	{
 		super(gp);
@@ -22,6 +27,11 @@ public class StartSword extends ItemEntity
 			e.printStackTrace();
 		}
 	}
+	/*
+	Constructor that creates a starting sword at a specific location on the room
+	@param GamePanel gp, access the main GamePanel
+	@param int x,y position 
+	*/
 	public StartSword(GamePanel gp, int x, int y)
 	{
 		super(gp);
