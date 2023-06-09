75ad14d6bf2896d7f831f78a05a190812d0f6a72
CycloneP26 <70384658+CycloneP26@users.noreply.github.com>
2023-06-08 22:00:05 -0400

    Add files via upload

--- src/object/HeartContainer.java
+++ src/object/HeartContainer.java
@@ -37,4 +37,22 @@ public class HeartContainer extends ItemEntity
 		setWorldX(x);
 		setWorldY(y);
 	}
+	public HeartContainer(GamePanel gp, int x, int y, boolean shop)
+	{
+		super(gp);
+		setName("heart");
+		try
+		{
+			setImage(ImageIO.read(getClass().getResourceAsStream("/objects/New_Piskel.png")));
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		setWorldX(x);
+		setWorldY(y);
+		setCost(20);
+		setShop(shop);
+	}
 }
+
--- src/object/ItemEntity.java
+++ src/object/ItemEntity.java
@@ -3,8 +3,11 @@ package object;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 import Main.GamePanel;
+import Main.UtilityTool;
 /*
 This class encompasses all items that appear on the map. It is the superclass 
 for all items and it is used to draw and keep track of them.
@@ -28,6 +31,12 @@ public class ItemEntity
 	private int solidAreaIDefX = 0;
 	private int solidAreaIDefY = 0;
 	
+	private int fireCount = 0;
+	private int spriteCounter = 0;
+	private BufferedImage fire1, fire2;
+	private boolean shop;
+	private int cost;
+	
 	/*Superconstructor that takes a GamePanel for the item to access gp
 	@param GamePanel gp to access gp
 	*/
@@ -39,9 +48,48 @@ public class ItemEntity
 	Draws the item at the position that it is given
 	@param Graphics2D g2 is required to draw the item on the GamePanel 
 	*/
+	public void update()
+	{
+		spriteCounter++;
+		if(spriteCounter > 15)
+		{
+			fireCount = 1;
+		}
+		if(spriteCounter > 30)
+		{
+			fireCount = 0;
+			spriteCounter = 0;
+		}
+	}
+	public void drawFire(Graphics2D g2)
+	{
+		if(fireCount == 0)
+		{
+			g2.drawImage(fire1, worldX, worldY, gp.getTileSize(), gp.getTileSize(), null);
+		}
+		else if (fireCount == 1)
+		{
+			g2.drawImage(fire2, worldX, worldY, gp.getTileSize(), gp.getTileSize(), null);
+		}
+	}
 	public void draw(Graphics2D g2)
 	{
 		g2.drawImage(image, worldX, worldY, gp.getTileSize(), gp.getTileSize(), null);
+	}
+	public BufferedImage setup(String imagePath,int width,int height) //image pathing so is works properly with gamePanel
+	{
+		UtilityTool uTool=new UtilityTool();
+		BufferedImage image=null;
+		try
+		{
+			image=ImageIO.read(getClass().getResourceAsStream(imagePath+".png"));
+			image=uTool.scaleImage(image,width,height);
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		return image; //returns the image after it is pathed correctly
 	}
 	public BufferedImage getImage() {
 		return image;
@@ -90,5 +138,33 @@ public class ItemEntity
 	}
 	public void setSolidAreaIDefY(int solidAreaIDefY) {
 		this.solidAreaIDefY = solidAreaIDefY;
+	}
+	public BufferedImage getFire1() {
+		return fire1;
+	}
+	public void setFire1(BufferedImage fire1) {
+		this.fire1 = fire1;
+	}
+	public BufferedImage getFire2() {
+		return fire2;
+	}
+	public void setFire2(BufferedImage fire2) {
+		this.fire2 = fire2;
+	}
+
+	public boolean isShop() {
+		return shop;
+	}
+
+	public void setShop(boolean shop) {
+		this.shop = shop;
+	}
+
+	public int getCost() {
+		return cost;
+	}
+
+	public void setCost(int cost) {
+		this.cost = cost;
 	}
 }
